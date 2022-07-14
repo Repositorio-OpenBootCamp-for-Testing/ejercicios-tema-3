d9b4307327be3a025ee513fe3491c0fd794f8aaa
Andre Salvador <junior.salvador.z@outlook.com>
2022-07-14 00:00:57 -0500

    Resolución de ejercicio v.2

--- src/com/ejercicio/Tema_3.java
+++ src/com/ejercicio/Tema_3.java
@@ -2,27 +2,36 @@ package com.ejercicio;
 
 public class Tema_3 {
     public static void main(String[] args) {
-        // Primera parte
+        /***************
+        * Primera parte
+        ****************/
         int sumaNumeros = Suma(10, 20, 30);
         System.out.println("(1ra parte) La suma es: " + sumaNumeros);
 
-        // Segunda parte
+        /***************
+        * Segunda parte (Ejecutaremos 2 incrementos)
+        ***************/
         Coche miCoche = new Coche();
+
+        // 1er incremento
+        miCoche.IncrementarPuertas();
+        System.out.println("(2da parte) El número de puertas es: " + miCoche.numPuertas);
+        // 2do incremento
         miCoche.IncrementarPuertas();
         System.out.println("(2da parte) El número de puertas es: " + miCoche.numPuertas);
     }
 
     // Función Suma de la Primera parte
-    public static int Suma(int a, int b, int c){
+    public static int Suma(int a, int b, int c) {
         return a + b + c;
     }
+}
 
-    // Clase Coche se la Segunda parte
-    static class Coche {
-        public int numPuertas = 0;
+// Clase Coche de la Segunda parte
+class Coche {
+    public int numPuertas = 0;
 
-        public void IncrementarPuertas(){
-            numPuertas++;
-        }
+    public void IncrementarPuertas() {
+        this.numPuertas++;
     }
 }
